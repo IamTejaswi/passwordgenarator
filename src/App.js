f9fd4aaed2ea53fc18306c7a9b1131a1d2cd1217
Tejaswi <201220014@nitdelhi.ac.in>
2024-06-07 12:23:26 +0530

    functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState,useCallback , useEffect  ,useRef} from "react";
+export default function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  
+  const passwordRef = useRef(null)
 
-function App() {
+  const generatePassword= useCallback(()=>{
+   let pass=''
+   let str="ASDFGHJKLPOIUYTREWQZXCVBNMasdfghjklpoiuytrewqzxcvbnm"
+   if(numberAllowed) str += "0123456789"
+   if(charAllowed) str += "!@#$%^&*()_+"
+
+   for(let i=1; i<length; i++){
+    const char = Math.floor(Math.random() * str.length + 1)
+    pass += str.charAt(char)
+   }
+   setPassword(pass)
+  },[length,numberAllowed,charAllowed])
+  
+
+  const copyPassword =()=>{
+  window.navigator.clipboard.writeText(password)
+  passwordRef.current?.select()
+  }
+  useEffect(() => {
+    generatePassword()
+  }, [length, numberAllowed, charAllowed]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Password genarator</h1>
+      <input placeholder="Password" value={password} type="text" />
+      <button onClick={copyPassword}>Copy</button>
+
+      <div>
+        <input
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+          ref={passwordRef}
+        />
+        <label>Length:{length} </label>
+      </div>
+      <div>
+        <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          onChange={() => {
+            setNumberAllowed((prev) => !prev);
+          }}
+        />
+        <label>Numbers</label>
+        <input
+          type="checkbox"
+          defaultChecked={charAllowed}
+          onChange={() => {
+            setCharAllowed((prev) => !prev);
+          }}
+        />
+        <label>Character</label>
+      </div>
+    </>
   );
 }
-
-export default App;
